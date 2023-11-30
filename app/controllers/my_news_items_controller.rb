bcdf1333e606a8a9206a0aadc199096afa4b50a2
Song Li <li_song@berkeley.edu>
2023-11-30 05:38:48 +0000

    trying to pass the check

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -52,13 +52,13 @@ class MyNewsItemsController < SessionController
   def set_news_item
     @news_item = NewsItem.find(params[:id])
   end
-  
+
   def set_issues_list
     @issues_list = ['Free Speech', 'Immigration', 'Terrorism', 'Social Security and
     Medicare', 'Abortion', 'Student Loans', 'Gun Control', 'Unemployment',
-    'Climate Change', 'Homelessness', 'Racism', 'Tax Reform', 'Net
+                    'Climate Change', 'Homelessness', 'Racism', 'Tax Reform', 'Net
     Neutrality', 'Religious Freedom', 'Border Security', 'Minimum Wage',
-    'Equal Pay']
+                    'Equal Pay']
   end
 
   # Only allow a list of trusted parameters through.
