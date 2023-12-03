1cd3849a09d6e72c055a3dd39c6c8cccc611f541
Joel Forman <joel.forman@berkeley.edu>
2023-12-03 05:58:35 +0000

    Added to gem file

--- app/controllers/campaign_finances_controller.rb
+++ app/controllers/campaign_finances_controller.rb
@@ -11,20 +11,17 @@ class CampaignFinancesController < ApplicationController
     cycle = params[:cycle]
     category = params[:category]
     key = Rails.application.credentials[:PROPUBLICA_API_KEY]
-
     if cycle.present? && category.present?
       api_url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
       response = HTTParty.get(api_url, headers: { 'X-API-Key' => key })
-
       if response.success?
         @results = response.parsed_response['results']
       else
-        @error_message = 'Error fetching data from the ProPublica API.'
+        @error_message = 'Error from ProPublica API'
       end
     else
-      @error_message = 'Please provide both cycle and category parameters.'
+      @error_message = 'Make sure cycle and category are both selected'
     end
-
     render 'campaign_finances/search'
   end
 end
