ebb7062895a7084f62d802dc6145185fd02756c1
Joel Forman <joel.forman@berkeley.edu>
2023-12-03 05:48:01 +0000

    Fix for linter

--- app/controllers/campaign_finances_controller.rb
+++ app/controllers/campaign_finances_controller.rb
@@ -3,15 +3,19 @@
 require 'httparty'
 
 class CampaignFinancesController < ApplicationController
-  def index end
+  def index
+    # pass
+  end
 
   def search
     cycle = params[:cycle]
     category = params[:category]
     key = Rails.application.credentials[:PROPUBLICA_API_KEY]
+
     if cycle.present? && category.present?
       api_url = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
       response = HTTParty.get(api_url, headers: { 'X-API-Key' => key })
+
       if response.success?
         @results = response.parsed_response['results']
       else
@@ -20,6 +24,7 @@ class CampaignFinancesController < ApplicationController
     else
       @error_message = 'Please provide both cycle and category parameters.'
     end
+
     render 'campaign_finances/search'
   end
 end
