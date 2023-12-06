31591ddd3638f32526e62f94c78533ff50d41ecb
Song Li <li_song@berkeley.edu>
2023-12-06 07:32:27 +0000

    add the test at login_controller

--- spec/login_controller_spec.rb
+++ spec/login_controller_spec.rb
@@ -3,6 +3,51 @@
 require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
+  let(:user_info) do
+    {
+      'provider' => 'google_oauth2',
+      'uid'      => '123456',
+      'info'     => {
+        'first_name' => 'John',
+        'last_name'  => 'Doe',
+        'email'      => 'john.doe@example.com'
+      }
+    }
+  end
+
+  describe 'POST #google_oauth2' do
+    before do
+      request.env['omniauth.auth'] = user_info
+      allow(User).to receive(:find_by).and_return(nil)
+      allow(controller).to receive(:create_google_user).with(user_info).and_call_original
+    end
+
+    it 'creates a new user session for Google user' do
+      post :google_oauth2
+      expect(User).to have_received(:find_by)
+      expect(controller).to have_received(:create_google_user).with(user_info)
+      expect(session[:current_user_id]).not_to be_nil
+      expect(response).to redirect_to(root_url)
+    end
+  end
+
+  describe 'POST #github' do
+    before do
+      modified_user_info = user_info.merge('provider' => 'github')
+      request.env['omniauth.auth'] = modified_user_info
+      allow(User).to receive(:find_by).and_return(nil)
+      allow(controller).to receive(:create_github_user).with(modified_user_info).and_call_original
+    end
+
+    it 'creates a new user session for GitHub user' do
+      post :github
+      expect(User).to have_received(:find_by)
+      expect(controller).to have_received(:create_github_user).with(user_info.merge('provider' => 'github'))
+      expect(session[:current_user_id]).not_to be_nil
+      expect(response).to redirect_to(root_url)
+    end
+  end
+
   describe 'GET #login' do
     it 'renders the login template' do
       get :login
